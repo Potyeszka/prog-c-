3d972fc254d70ffe0ecb88a175f27f0c2d038f9b
Potyeszka <161585940+Potyeszka@users.noreply.github.com>
2024-10-16 17:18:17 +0200

    Add files via upload

---
+++ Program 5.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robotfasz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Útvonal adat = null;
+                string[] be = File.ReadAllLines("teszt.txt");
+                string[] adatok = new string[be.Length];
+                string[] karakterek = null;
+                for (int i = 0; i < be.Length; i++)
+                {
+                    string[] darabol = be[i].Split(',');
+                    karakterek = new string[darabol.Length];
+                    int id = int.Parse(darabol[0]);
+                    string forras = darabol[1];
+                    string cel = darabol[2];
+                    for (int j = 3; j < karakterek.Length; j++)
+                    {
+                        karakterek[j - 3] = darabol[j];
+                    }
+                    adat = new Útvonal(id, forras, cel, karakterek);
+                    Console.WriteLine("Módosítatlan: "+adat);
+                    if (adat.Módosít(1, "R"))
+                    {
+                        Console.WriteLine("A módosítás megtörtént!");
+                        Console.WriteLine("Módosított: "+adat);
+                    }
+                    if (Útvonal.Hibás_e(adat)) { Console.WriteLine("Szerepel benne hiba"); } else { Console.WriteLine("Nem szerepel benne hiba"); }
+                    //Leghosszab sor
+                    int sor = 0, seged = 0;
+                    for (int k = 0; k < karakterek.Length; k++)
+                    {
+                        if (karakterek[k] == "F")
+                        {
+                            sor++;
+                        }
+                        else
+                        {
+                            if (sor > seged)
+                            {
+                                seged = sor;
+                                sor = 0;
+                            }
+                            else
+                            {
+                                sor = 0;
+                            }
+                        }
+                    }
+                    Console.WriteLine(adat + "" +" A leghosszabb F sorozat: " +seged);
+
+                }
+
+
+
+
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Útvonal 1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robotfasz
+{
+    internal class Útvonal
+    {
+        public int id;
+        public string forrás, cél;
+        public string[] kód;
+        public int hibák;
+
+        public Útvonal(int id, string forrás, string cél, string[] kód)
+        {
+            this.id = id;
+            this.forrás = forrás;
+            this.cél = cél;
+            this.kód = kód;
+            for (int i = 0; i < kód.Length; i++)
+            {
+                if (kód[i]!= "F" && kód[i]!="R" && kód[i]!="L" && kód[i]!=null)
+                {
+                    kód[i] = "N";
+                    hibák++;
+                }
+            }
+
+        }
+
+        public override string ToString()
+        {
+            string s = id+","+forrás+","+cél+",";
+           foreach(var adat in kód)
+            {
+                if(adat!=null)
+                {
+                    s += adat + ",";
+                }
+            }
+            return s;
+        }
+
+        public bool Módosít(int az, string karakter)
+        {
+            if(az > kód.Length)
+            {
+                return false;
+            }else {kód[az] = karakter; return true;}
+           
+        }
+        public static bool Hibás_e(Útvonal ú)
+        {
+            if(ú.hibák > 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+    }
+}
