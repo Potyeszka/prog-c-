7bf315ddce5b4aa51988f23ffeb409be2f54f3ff
Potyeszka <161585940+Potyeszka@users.noreply.github.com>
2024-11-07 09:22:55 +0100

    Add files via upload

---
+++ gyak8/gyak8/gyak8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            versenyző v = new versenyző(12, "Kis Ida");
+            
+        }
+    }
+}
---
+++ gyak8/gyak8/gyak8/versenyző.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak8
+{
+    public class versenyző:IComparable<versenyző>
+    {
+        private int id;
+        private string nev;
+        private List<int> eredmenyek;
+
+        public versenyző(int id, string nev)
+        {
+            this.id = id;
+            this.nev = nev;
+            this.eredmenyek = new List<int>();
+        }
+
+        public versenyző()
+        {
+            this.eredmenyek = new List<int>();
+        }
+
+        public override string ToString()
+        {
+            string s=$"{id} {nev}, eredményei: ";
+            foreach (var e in eredmenyek)
+            {
+                s+=e + ", ";
+            }
+            s += $"legjobb: {Legjobbugras()}";
+            return s;
+        }
+        public void SetId(int id)
+        { this.id = id; }
+
+        public void SetNev(string nev)
+        {  this.nev = nev; }
+
+        
+
+        public int Legjobbugras()
+        {
+            return eredmenyek.Max();
+
+        }
+
+
+
+        public int CompareTo(versenyző v)
+        {
+            return Legjobbugras().CompareTo(v.Legjobbugras());
+        }
+    }
+}
